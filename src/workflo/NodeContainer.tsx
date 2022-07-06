1858440bebc6f4af0147540ac68146e269f046b6
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2022-07-06 16:36:45 +0100

    drawing basic connectors

--- src/workflo/NodeContainer.tsx
+++ src/workflo/NodeContainer.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import { Node } from './Node';
 
 import { NodeType, ChildNode } from "./workflo";
 
+/**
+ * The connector target offsets type keyed on connector id.
+ */
+export type ConnectorTargetOffsets = { [key: string]: number };
 
 /**
  * The NodeContainer component props.
@@ -18,6 +22,42 @@ export type NodeContainerProps = {
  * The NodeContainer component.
  */
  export const NodeContainer: React.FunctionComponent<NodeContainerProps> = ({ parentNode, childNodes, nodeComponents }) => {
+    // The ref to the node children container.
+    const nodeChildrenContainerRef = useRef<HTMLDivElement>(null);
+
+    const [connectorTargetOffsets, setConnectorTargetOffsets] = useState<number[] | null>(null);
+    const [nodeChildrenContainerHeight, setNodeChildrenContainerHeight] = useState<number>(0);
+
+    useEffect(() => {
+        const currentNodeChildrenContainerHeight = nodeChildrenContainerRef.current?.clientHeight || 0;
+
+        // Work out the connector target offsets based on the heights and positions of each child node.
+        if (!connectorTargetOffsets || currentNodeChildrenContainerHeight != nodeChildrenContainerHeight) {
+            const offsets = [];
+
+            let childPositionOffset = 0;
+
+            for (let childIndex = 0; childIndex < (nodeChildrenContainerRef.current?.children.length || 0); childIndex++) {
+                // Get the current child element.
+                const childElement = nodeChildrenContainerRef.current?.children[childIndex]!;
+
+                // Get the height of the child.
+                const childHeight = childElement.clientHeight;
+
+                // Calculate the end point of the connector, which should be aligned with the child element.
+                const childConnectorOffset = childPositionOffset + (childHeight / 2);
+
+                offsets.push(childConnectorOffset);
+
+                // Add the child height to the offset.
+                childPositionOffset += childHeight;
+            }
+
+            setConnectorTargetOffsets(offsets);
+            setNodeChildrenContainerHeight(currentNodeChildrenContainerHeight);
+        }
+    });
+
     return (
         <div className="workflow-canvas-node-container">
             <div className="workflow-canvas-node-parent-container">
@@ -25,9 +65,12 @@ export type NodeContainerProps = {
             </div>
             <div className="workflow-canvas-node-connector-svg-wrapper">
                 <svg className="workflow-canvas-node-connector-svg">
+                    {connectorTargetOffsets && connectorTargetOffsets.map((offset, index) =>
+                        <line x1="0" y1="50%" x2="100%" y2={`${offset}`} stroke="#ff0000" stroke-width="1" stroke-linecap="round"></line>
+                    )}
                 </svg>
             </div>
-            <div className="workflow-canvas-node-children-container">
+            <div ref={nodeChildrenContainerRef} className="workflow-canvas-node-children-container">
                 {childNodes.map((childNode) =>
                     <NodeContainer parentNode={childNode.child.node} childNodes={childNode.child.children} nodeComponents={nodeComponents} />
                 )}
