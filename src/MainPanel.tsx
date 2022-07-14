69c4fac2c0dc58c7cf47fcbb98142f0b432460ba
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2022-07-14 12:13:31 +0100

    Adding fabs

--- src/MainPanel.tsx
+++ src/MainPanel.tsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import { WorkflowCanvas, WorkflowCanvasInstance } from "./workflo/WorkflowCanvas";
 import { DefaultNode } from "./workflo/DefaultNode";
 import { NodeType, ConnectorType } from "./workflo/workflo";
+import Fab from "@mui/material/Fab/Fab";
+
+import './MainPanel.css';
 
 export type CanvasElements = { nodes: NodeType[], edges: ConnectorType[] };
   
@@ -28,14 +31,22 @@ export type MainPanelProps = {
     });
 
     return (
-        <WorkflowCanvas
-            onInitalise={(instance) => setCanvasInstance(instance)}
-            nodes={elements.nodes}
-            connectors={elements.edges}
-            nodeComponents={{
-                "default": DefaultNode
-            }}
-        />
+        <div className="main-panel">
+            <WorkflowCanvas
+                onInitalise={(instance) => setCanvasInstance(instance)}
+                nodes={elements.nodes}
+                connectors={elements.edges}
+                nodeComponents={{
+                    "default": DefaultNode
+                }}
+            />
+            <div className="main-panel-fab-container">
+                <Fab className="run-tree-fab main-panel-fab" size="medium" color="primary">
+                </Fab>
+                <Fab className="run-tree-fab main-panel-fab" size="medium" color="primary">
+                </Fab>
+            </div>
+        </div>
     );
   }
   
