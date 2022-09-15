7e6e446ccdf1e1f0103629b3f81eabc394b13891
Nikolas Howard <nikolas.howard@infor.com>
2022-09-15 18:06:58 +0100

    updating examples

--- src/Examples.tsx
+++ src/Examples.tsx
@@ -16,16 +16,7 @@ export type Example = {
 /**
  * An array of example definition and board commbinations.
  */
-export const Examples: Example[] = [
-    {
-        caption: "Empty",
-        category: "misc",
-        definition: `root {
-}`,
-        board: `class Board {
-}`
-    },
-    
+export const Examples: Example[] = [    
     {
         caption: "Sorting Lunch",
         category: "misc",
@@ -60,7 +51,7 @@ export const Examples: Example[] = [
         return true;
     }
     HasDollars(amount) {
-        return getNumberValue("How much money do we have?") >= amount;
+        return getNumberValue("How many dollars do we have?") >= amount;
     }
     HasIngredient(ingredient) {
         return getBooleanValue(\`Do we have \${ingredient}?\`);
@@ -90,6 +81,219 @@ export const Examples: Example[] = [
     SomeAction() {
         showInfoToast("Action Completed!");
         return State.SUCCEEDED;
+    }
+}`
+    },
+
+    {
+        caption: "Action with arguments",
+        category: "leaf",
+        definition: `root {
+    action [Say, "hello world", 5, true]
+}`,
+        board: `class Board {
+    Say(dialog, times = 1, shout = false) {
+        for (var index = 0; index < times; index++) {
+            showInfoToast(shout ? dialog.toUpperCase() + "!!!" : dialog);
+        }
+        return State.SUCCEEDED;
+    }
+}`
+    },
+
+    {
+        caption: "Asynchronous Action",
+        category: "leaf",
+        definition: `root {
+    action [SomeAsyncAction]
+}`,
+        board: `class Board {
+    SomeAsyncAction() {
+        return new Promise(function(resolve, reject) {
+            setTimeout(function() {
+                resolve(State.SUCCEEDED);
+            }, 3000);
+        });
+    }
+}`
+    },
+
+    {
+        caption: "Condition",
+        category: "leaf",
+        definition: `root {
+    condition [SomeCondition]
+}`,
+        board: `class Board {
+    SomeCondition() {
+        return true;
+    }
+}`
+    },
+
+    {
+        caption: "Condition with arguments",
+        category: "leaf",
+        definition: `root {
+    condition [HasItem, "gold", 500]
+}`,
+        board: `class Board {
+    HasItem(item, quantity = 1) {
+        return getBooleanValue(\`Do we have \${quantity} \${item}?\`);
+    }
+}`
+    },
+
+    {
+        caption: "Wait",
+        category: "leaf",
+        definition: `root {
+    wait [2000]
+}`,
+        board: `class Board {}`
+    },
+
+    {
+        caption: "Wait for one-five seconds",
+        category: "leaf",
+        definition: `root {
+    wait [1000, 5000]
+}`,
+        board: `class Board {}`
+    },
+
+    {
+        caption: "Sequence",
+        category: "composite",
+        definition: `root {
+    sequence {
+        action [Walk]
+        wait [1000]
+        action [Fall]
+        wait [1000]
+        action [Laugh]
+    }
+}`,
+        board: `class Board {
+    Walk() {
+        showInfoToast("Walking!");
+        return State.SUCCEEDED;
+    }
+    Fall() {
+        showInfoToast("Falling!");
+        return State.SUCCEEDED;
+    }
+    Laugh() {
+        showInfoToast("Laughing!");
+        return State.SUCCEEDED;
+    }
+}`
+    },
+
+    {
+        caption: "Selector",
+        category: "composite",
+        definition: `root {
+    selector {
+        action [TeleportHome]
+        action [FlyHome]
+        action [DriveHome]
+        action [WalkHome]
+    }
+}`,
+        board: `class Board {
+    TeleportHome() {
+        showErrorToast("We cannot teleport home, the technology simply doesn't exist!");
+        return State.FAILED;
+    }
+    FlyHome() {
+        showErrorToast("We can't afford a plane ticket!");
+        return State.FAILED;
+    }
+    DriveHome() {
+        showInfoToast("We have a car, we drive home!");
+        return State.SUCCEEDED;
+    }
+    WalkHome() {
+        showInfoToast("We walk home, it took ages!");
+        return State.SUCCEEDED;
+    }
+}`
+    },
+
+    {
+        caption: "Lotto",
+        category: "composite",
+        definition: `root {
+    lotto {
+        action [PickPath, "left"]
+        action [PickPath, "right"]
+    }
+}`,
+        board: `class Board {
+    PickPath(direction) {
+        showInfoToast(\`We picked the \${direction} path!\`);
+        return State.SUCCEEDED;
+    }
+}`
+    },
+
+    {
+        caption: "Weighted Lotto",
+        category: "composite",
+        definition: `root {
+    lotto [20,10,3,1] {
+        action [CommonAction]
+        action [UncommonAction]
+        action [RareAction]
+        action [VeryRareAction]
+    }
+}`,
+        board: `class Board {
+    CommonAction() {
+        showInfoToast("We very often do this action!");
+        return State.SUCCEEDED;
+    }
+    UncommonAction() {
+        showInfoToast("We sometimes do this action!");
+        return State.SUCCEEDED;
+    }
+    RareAction() {
+        showInfoToast("We do not do this action very often!");
+        return State.SUCCEEDED;
+    }
+    VeryRareAction() {
+        showInfoToast("We hardly ever do this action!");
+        return State.SUCCEEDED;
+    }
+}`
+    },
+
+    {
+        caption: "Parallel",
+        category: "composite",
+        definition: `root {
+    parallel {
+        sequence {
+            wait[500, 2500]
+            wait[500, 2500]
+            wait[500, 2500]
+            action [Succeed]
+        }
+        sequence {
+            wait[500, 2500]
+            wait[500, 2500]
+            wait[500, 2500]
+            action [Fail]
+        }
+    }
+}`,
+        board: `class Board {
+    Succeed() {
+        return State.SUCCEEDED;
+    }
+    Fail() {
+        return State.FAILED;
     }
 }`
     },
